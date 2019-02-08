7042e30d28bf64405eb5cde47c00b9462f6b5867
Roberto Mardeni <rmardeni@gmail.com>
2019-02-07 22:30:56 -0500

    Changed to copy files to 3dsMax Plugins directory

--- ncj/3dsmax/scripts/install-scalpel-plugin.ps1
+++ ncj/3dsmax/scripts/install-scalpel-plugin.ps1
@@ -6,15 +6,10 @@ $init_file = Join-Path $env:AZ_BATCH_NODE_SHARED_DIR "init_scalpel_plugin.txt"
 If (!(Test-Path $init_file)) {
   Write-Host "Installing Scalpel Plugins"
   try {
-    $plugins_path = "C:\Users\Public\Documents"
-    $scalpel_path = "$plugins_path\cebas"
     $scalpel_package_path = [System.Environment]::GetEnvironmentVariable("AZ_BATCH_APP_PACKAGE_SCALPEL_3DSMAX#2019")
 
-    # Copy the Scalpel files to the expected location
-    Copy-Item -Path "$scalpel_package_path\cebas" -Destination $plugins_path -Recurse -Force -Verbose
-
-    # Add the Scalpel path to the plugin.ini
-    Add-Content -Path (Join-Path $env:3DSMAX_2019 "plugin.ini") -Value $scalpel_path
+    # Copy the Scalpel files to the 3dsMax Plugins directory
+    Copy-Item -Path "$scalpel_package_path\cebas" -Destination "$env:3DSMAX_2019\Plugins" -Recurse -Force -Verbose
 
     "done" | Out-File $init_file
   } catch {
