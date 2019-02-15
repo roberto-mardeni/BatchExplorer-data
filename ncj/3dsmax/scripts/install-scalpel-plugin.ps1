9f66e3ed260ce21c5ff3784cfcc96959d89bb5b4
Roberto Mardeni <rmardeni@gmail.com>
2019-02-15 09:05:08 -0500

    Added support for scalpel plugin

--- ncj/3dsmax/scripts/install-scalpel-plugin.ps1
+++ ncj/3dsmax/scripts/install-scalpel-plugin.ps1
@@ -6,10 +6,15 @@ $init_file = Join-Path $env:AZ_BATCH_NODE_SHARED_DIR "init_scalpel_plugin.txt"
 If (!(Test-Path $init_file)) {
   Write-Host "Installing Scalpel Plugins"
   try {
-    $scalpel_package_path = [System.Environment]::GetEnvironmentVariable("AZ_BATCH_APP_PACKAGE_SCALPEL_3DSMAX#2019")
+    # User Settings Path = C:\Users\seel016\AppData\Local\Autodesk\3dsMax\2019 - 64bit\ENU
+    $plugins_path = "C:\Users\Public\Documents\"
 
-    # Copy the Scalpel files to the 3dsMax Plugins directory
-    Copy-Item -Path "$scalpel_package_path\cebas" -Destination "$env:3DSMAX_2019\Plugins" -Recurse -Force -Verbose
+    # Extract and install Corona Plugin for 3ds Max 2018
+    7z x -y -o"$plugins_path" "cebas.zip"
+    Write-Host "Installed Scalpel Plugin"
+    
+    # Copy the Plugin.ini file to the 3dsMax directory
+    Copy-Item -Path "Plugin.ini" -Destination "$env:3DSMAX_2019" -Force -Verbose
 
     "done" | Out-File $init_file
   } catch {
